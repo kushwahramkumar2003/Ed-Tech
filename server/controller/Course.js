636aa8820d39629a6d4e06a242b1eb76896183a8
Ramkumar kushwah <kushwahramkumar2003@gmail.com>
2023-08-03 15:01:46 +0530

    commit

---
+++ server/controller/Course.js
@@ -0,0 +1,19 @@
+const Course = require("../models/Course");
+const Tag = require("../models/Tag");
+const User = require("../models/User");
+const { uploadImageToCloudinary } = require("../utils/imageUploader");
+
+//createCourse hadler function
+
+exports.createCourse = async (req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+
+
+//getAllCourse handler  function
--- server/controller/Tags.js
+++ server/controller/Tags.js
@@ -17,6 +17,30 @@ exports.createTag = async (req, res) => {
       name: name,
       description: description,
     });
+    console.log(tagDetails);
+    // return response
+    return res.status(200).json({
+      success: true,
+      message: "Tag Created Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//getAllTags handler function
+
+exports.showAlltags = async (req, res) => {
+  try {
+    const allTags = await Tag.find({}, { name: true, description: true });
+    return res.status(200).json({
+      success: true,
+      message: "All tags returned successfully",
+      allTags,
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
