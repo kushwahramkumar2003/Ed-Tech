69bda5274901a923b5585b9cc4ae5bd5795f8c5b
Ramkumar kushwah <kushwahramkumar2003@gmail.com>
2023-08-06 22:25:52 +0530

    commit

--- server/controller/Course.js
+++ server/controller/Course.js
@@ -100,7 +100,7 @@ exports.createCourse = async (req, res) => {
 
 //getAllCourse handler  function
 
-exports.showAllCourses = async (req, res) => {
+exports.getAllCourses = async (req, res) => {
   try {
     //TOD: change the below syatement inc.
     const allCourses = await Course.find(
@@ -127,6 +127,51 @@ exports.showAllCourses = async (req, res) => {
       success: false,
       message: "Cannot fetch coures data",
       error: error.message,
+    });
+  }
+};
+
+//get CourseDetails
+exports.getCourseDetails = async (req, res) => {
+  try {
+    //get id
+    const { courseId } = req.body;
+    //find course details
+    const courseDetails = await Course.find({
+      _id: courseId,
+    })
+      .populate({
+        path: "instructor",
+        populate: {
+          path: "additionalDetails",
+        },
+      })
+      .populate("category")
+      .populate("ratingAndreviews")
+      .populate({
+        path: "courseContent",
+        populate: {
+          path: "subSection",
+        },
+      })
+      .exec();
+    //validation
+    if (!courseDetails) {
+      return res.status(400).json({
+        success: false,
+        message: `Could not find the course with ${courseId}`,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Course Details fetched successfully",
+      data: courseDetails,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
     });
   }
 };
---
+++ server/controller/SubSection.js
@@ -0,0 +1,61 @@
+const Section = require("../models/Section");
+const SubSection = require("../models/SubSection");
+
+//create SubSection
+
+exports.createSubSection = async (req, res) => {
+  try {
+    //fetch data from Req body
+    const { sectionId, title, timeDuration, description } = req.body;
+    //extract file/video
+    const video = req.files.videoFile;
+
+    //validation
+    if (!sectionId || !title || !timeDuration || !description || !video) {
+    }
+    {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required ",
+      });
+    }
+    //upload video to cloudinary
+    const uploadDetails = await uploadImageToCloudinary(
+      video,
+      process.env.FOLDER_NAME
+    );
+    //create subsection
+    const subSectionDetails = await SubSection.create({
+      title: title,
+      timeDuration: timeDuration,
+      description: description,
+      videoUrl: uploadDetails.secure_url,
+    });
+    //update section with this sub section ObjectId
+    const updatedSection = await Section.findByIdAndUpdate(
+      { _id: sectionId },
+      {
+        $push: {
+          subSection: subSectionDetails._id,
+        },
+      },
+      { new: true }
+    );
+    //HW: log updated section here, after adding populate query
+    //return response
+    return res.status(200).json({
+      success: true,
+      message: "Sub Section created Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+// HW: updateSubSection 
+
+// HW: 
