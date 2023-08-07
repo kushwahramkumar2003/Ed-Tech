697e522d11bcd11e1025f4cdfe651d3c486726c5
Ramkumar kushwah <kushwahramkumar2003@gmail.com>
2023-08-07 13:56:04 +0530

    commit

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-exports.connect = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("DB Connected Successfully");
-  } catch (err) {
-    console.log("DB Connection Failed");
-    console.log(err);
-    process.exit(1);
-  }
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+    })
+    .then(() => console.log("DB Connected Successfully"))
+    .catch( (error) => {
+        console.log("DB Connection Failed");
+        console.error(error);
+        process.exit(1);
+    } )
 };
--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,23 +1,33 @@
 const nodemailer = require("nodemailer");
+require('dotenv').config()
+
+
 const mailSender = async (email, title, body) => {
-  try {
-    let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-    });
-    let info = await transporter.sendMail({
-      from: "StudyNotion || CodeHelp - by Babbar",
-      to: `${email}`,
-      subject: `${title}`,
-      html: `${body}`,
-    });
-    console.log(info);
-    return info;
-  } catch (error) {
-    console.log(error.message);
-  }
-};
+    try{
+            let transporter = nodemailer.createTransport({
+                host:process.env.MAIL_HOST,
+                port: 587,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS,
+                }
+            })
+
+
+            let info = await transporter.sendMail({
+                from: `"Study Notion" <${process.env.MAIL_USER}>`,
+                to:`${email}`,
+                subject: `${title}`,
+                html: `${body}`,
+            })
+            console.log(info);
+            return info;
+    }
+    catch(error) {
+        console.log(error.message);
+        return error;
+    }
+}
+
+
 module.exports = mailSender;
