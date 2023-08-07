2d91cc823ea1fd104709fdcf8cf6b8d51c80f9c4
Ramkumar kushwah <kushwahramkumar2003@gmail.com>
2023-08-07 16:16:12 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,24 +1,26 @@
-const express=require('express');
+const express = require("express");
 
-const app=express();
+const app = express();
 
-const userRoutes=require('./routes/User');
-const paymentRoutes=require('./routes/Payments');
-const profileRoutes=require('./routes/Profile');
-const courseRoutes=require('./routes/Course');
+const userRoutes = require("./routes/User");
+const paymentRoutes = require("./routes/Payments");
+const profileRoutes = require("./routes/Profile");
+const courseRoutes = require("./routes/Course");
 
-const database=require('./config/database');
-const cookieParser=require("cookie-parser")
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
 
-const cors=require('cors');
-const fileUpload=require("express-fileupload");
-const {cloudnairyconnect}=require('./config/cloudinary');
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const { cloudnairyconnect } = require("./config/cloudinary");
 
-const dotenv=require("dotenv")
+const dotenv = require("dotenv");
 dotenv.config();
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
+// database connect
 database.connect();
+// middlewares
 
 app.use(express.json());
 app.use(cookieParser());
@@ -29,32 +31,40 @@ app.use(cookieParser());
 //         credentials: true,
 //     }
 // ));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
-app.use(fileUpload(
-    {
-        useTempFiles: true,
-        tempFileDir: "/tmp"
-    }
-));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
 
+//cloudinary connection
 cloudnairyconnect();
 
-app.use('/api/v1/auth',userRoutes);
+//routes
+app.use("/api/v1/auth", userRoutes);
 
-app.use('/api/v1/payment',paymentRoutes);
+app.use("/api/v1/payment", paymentRoutes);
 
-app.use('/api/v1/profile',profileRoutes);
+app.use("/api/v1/profile", profileRoutes);
 
-app.use('/api/v1/course',courseRoutes);
+app.use("/api/v1/course", courseRoutes);
 
-app.use('/api/v1/contact',require('./routes/ContactUs'));
+app.use("/api/v1/contact", require("./routes/ContactUs"));
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Your server is up and running.....",
+  });
+});
 
-app.get("/",(req, res)=>{
-    res.status(200).json({
-        message:"Welcome to the API"
-    })});
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+let server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${server.address().port}`);
+});
